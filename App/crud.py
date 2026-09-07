c4f32b116a21cea030ea520e02d66e463462e92f
JoelAlfred-Higgs <joelalfred.29csa@licet.ac.in>
2026-09-07 21:10:40 +0530

    Handled duplicate note_id

--- App/crud.py
+++ App/crud.py
@@ -1,24 +1,27 @@
+from pydantic_core.core_schema import NoInfoWrapValidatorFunction
 from pracmodel import Note
 def getnote(note_id,db):
  return db.query(Note).filter(Note.note_id == note_id).first()
 
 def addnote(new_note,db):
+    if db.query(Note).filter(Note.note_id == new_note.note_id).first():
+        return None
     db.add(new_note)
     db.commit()
     db.refresh(new_note)
     return new_note
 
-def updatenote(Note,content,note_id,db):
+def updatenote(content,note_id,db):
     un = db.query(Note).filter(Note.note_id == note_id).first()
     if un:
-       un.content = content
+       un.content = content.content
        db.commit()
        db.refresh(un)
        return un
       
     return None
     
-def deletenote(Note,note_id,db):
+def deletenote(note_id,db):
     delete = db.query(Note).filter(Note.note_id == note_id).first()
     if delete is not None:
        db.delete(delete)
