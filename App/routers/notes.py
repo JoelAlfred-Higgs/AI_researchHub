414d5b1a6f711ce886e21da0e17f9f43af52f91a
JoelAlfred-Higgs <joelalfred.29csa@licet.ac.in>
2026-09-07 21:01:47 +0530

    Added valid exception for each method and removed manual response model object

--- App/routers/notes.py
+++ App/routers/notes.py
@@ -8,34 +8,34 @@ from DataBase import SessionLocal, engine, get_db
 note_router = APIRouter()
 Base.metadata.create_all(bind = engine) #creates a table in mysql
 
-@note_router.get("/{note_id}") #changed the endpoint to standard api endpoints
+@note_router.get("/{note_id}",response_model=NoteResponse,status_code=200) #changed the endpoint to standard api endpoints
 def get_note(note_id: int,db:SessionLocal = Depends(get_db)):
     find_note = getnote(note_id,db)
-    if find_note is not None:
-        response_model = NoteResponse(note_id = find_note.note_id , title = find_note.title, content = find_note.content)
-        return response_model 
-    raise HTTPException(status_code=404, detail="Note not found!")
-  
-@note_router.post("/{id}")
+    if find_note is None:
+       raise HTTPException(status_code=404, detail="Note not found!")
+    return find_note
+
+
+@note_router.post("/",response_model = NoteResponse,status_code=201)
 def add_note(note:CreateNote,db:SessionLocal = Depends(get_db)):
     new_note = Note(note_id = note.note_id,title = note.title,content = note.content)
     new_note = addnote(new_note,db)
     if new_note is not None:
-        response_model = NoteResponse(note_id = new_note.note_id , title = new_note.title, content = new_note.content)
-        return response_model 
-    return "Note added Successfully!"
+        return new_note
+    raise HTTPException(status_code=400, detail="Note not added!")
 
-@note_router.put("/{id}")
+@note_router.put("/{id}",response_model = NoteResponse,status_code=200)
 def update_note(id:int,content:UpdateNote,db:SessionLocal = Depends(get_db)):
     updated = updatenote(Note,content,id,db)
     if updated is not None:
-        response_model = NoteResponse(note_id = updated.note_id , title = updated.title, content = updated.content)
-        return response_model  
-    return "Note does not exist!"
-@note_router.delete("/{id}")
+        return updated
+    raise HTTPException(status_code=404, detail="Note not found!")
+
+
+@note_router.delete("/{id}",status_code=204)
 def delete_note(id:int,db:SessionLocal = Depends(get_db)):
     deleted = deletenote(Note,id,db)
     if deleted:
         return "Note deleted Successfully!"
-    return "Note does not exist!"
+    raise HTTPException(status_code=404, detail="Note not found!")
 
