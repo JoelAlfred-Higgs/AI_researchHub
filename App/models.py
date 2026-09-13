b7abde34e3336cfc565c03cd94053d9621860785
JoelAlfred-Higgs <joelalfred.29csa@licet.ac.in>
2026-09-13 10:44:27 +0530

    fixed foreign key bug

--- App/models.py
+++ App/models.py
@@ -14,19 +14,19 @@ class User(Base): #stores the details of the user
     #Back populates-link two object models together, update one object and linked object gets updated
 
 class Category(Base): #holds the category of notes
-    __tablename__ = "categories"
+    __tablename__ = "categories" 
     categ_id = Column(Integer,primary_key = True,index = True)
     categ_name = Column(String(100),nullable = False)
-    owner_id = Column(Integer,ForeignKey = "users.user_id")
-    user = relationship("User",back_populates = "category")
+    owner_id = Column(Integer,ForeignKey("users.user_id"))
+    user = relationship("User",back_populates="category")
     notes = relationship("Note",back_populates = "category")
 
-class Notes(Base):
+class Note(Base):
     __tablename__ = "notes" # core unit and contains the main content
     note_id  = Column(Integer,primary_key = True,index = True)
     title = Column(String(50),nullable = False)
     content  = Column(Text,nullable = False)
-    owner_id = Column(Integer,ForeignKey = "users.user_id")
-    category_id = Column(Integer,ForeignKey = "categories.categ_id")
+    owner_id = Column(Integer,ForeignKey("users.user_id"))
+    category_id = Column(Integer,ForeignKey("categories.categ_id"))
     category = relationship("Category",back_populates = "notes")
     user = relationship("User",back_populates = "notes")
