2af1a7a4c30c367d2a7aa96532b72ae345cadefc
JoelAlfred-Higgs <joelalfred.29csa@licet.ac.in>
2026-09-13 10:53:03 +0530

    No changes

--- App/DataBase.py
+++ App/DataBase.py
@@ -1,4 +1,4 @@
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
 db_url = "mysql+pymysql://root:Ajoel14%24@localhost:3306/AIresearchHub"
