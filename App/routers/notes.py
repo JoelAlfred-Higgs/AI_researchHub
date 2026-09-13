f78617ea998ba7d3b21479466cd89b7e5bb37fa3
JoelAlfred-Higgs <joelalfred.29csa@licet.ac.in>
2026-09-13 10:39:31 +0530

    Fixed bugs in the endpoint

--- App/routers/notes.py
+++ App/routers/notes.py
@@ -3,13 +3,14 @@ from fastapi import HTTPException,Depends
 from pracmodel import Note,CreateNote,UpdateNote,Base,NoteResponse
 from crud import getnote,addnote,updatenote,deletenote
 from DataBase import SessionLocal, engine, get_db
+from sqlalchemy.orm import Session
 
 
 note_router = APIRouter()
 Base.metadata.create_all(bind = engine) #creates a table in mysql
 
 @note_router.get("/{note_id}",response_model=NoteResponse,status_code=200) #changed the endpoint to standard api endpoints
-def get_note(note_id: int,db:SessionLocal = Depends(get_db)):
+def get_note(note_id: int,db:Session = Depends(get_db)):
     find_note = getnote(note_id,db)
     if find_note is None:
        raise HTTPException(status_code=404, detail="Note not found!")
@@ -17,7 +18,7 @@ def get_note(note_id: int,db:SessionLocal = Depends(get_db)):
 
 
 @note_router.post("/",response_model = NoteResponse,status_code=201)
-def add_note(note:CreateNote,db:SessionLocal = Depends(get_db)):
+def add_note(note:CreateNote,db:Session = Depends(get_db)):
     new_note = Note(note_id = note.note_id,title = note.title,content = note.content)
     new_note = addnote(new_note,db)
     if new_note is not None:
@@ -25,16 +26,16 @@ def add_note(note:CreateNote,db:SessionLocal = Depends(get_db)):
     raise HTTPException(status_code=409, detail="Note already exists!")
 
 @note_router.put("/{id}",response_model = NoteResponse,status_code=200)
-def update_note(id:int,content:UpdateNote,db:SessionLocal = Depends(get_db)):
-    updated = updatenote(Note,content,id,db)
+def update_note(id:int,content:UpdateNote,db:Session = Depends(get_db)):
+    updated = updatenote(content,id,db)
     if updated is not None:
         return updated
     raise HTTPException(status_code=404, detail="Note not found!")
 
 
 @note_router.delete("/{id}",status_code=204)
-def delete_note(id:int,db:SessionLocal = Depends(get_db)):
-    deleted = deletenote(Note,id,db)
+def delete_note(id:int,db:Session = Depends(get_db)):
+    deleted = deletenote(id,db)
     if deleted:
         return "Note deleted Successfully!"
     raise HTTPException(status_code=404, detail="Note not found!")
