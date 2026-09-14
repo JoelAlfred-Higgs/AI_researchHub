69385de2484192bb11d4ff2435bf3c1739d7ccb2
JoelAlfred-Higgs <joelalfred.29csa@licet.ac.in>
2026-09-14 11:32:42 +0530

    added a category router

--- App/main.py
+++ App/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from routers import notes
 from routers import users
+from routers import categories
 
 
 app = FastAPI()
-app.include_router(notes.note_router)
-app.include_router(users.user_router)
+app.include_router(notes.note_router,prefix="/notes")
+app.include_router(users.user_router,prefix="/users")
+app.include_router(categories.categ_router,prefix = "/categories")
