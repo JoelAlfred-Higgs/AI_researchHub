807f522439a017e69cd9e732531b50b6bd1f1fd4
JoelAlfred-Higgs <joelalfred.29csa@licet.ac.in>
2026-09-14 11:37:38 +0530

    changes commit message to post and added a comment

--- App/routers/categories.py
+++ App/routers/categories.py
@@ -1,12 +1,12 @@
 from crud import addcategory,getcategory
 from DataBase import engine,get_db
 from models import Category,Base
-from schemas import CreateCategory, CategoryResponse
+from schemas import CreateCategory, CategoryResponse,UpdateCategory
 from fastapi import APIRouter,HTTPException,Depends
 from sqlalchemy.orm import Session
 
 categ_router = APIRouter()
-Base.metadata.create_all(bind = engine)
+Base.metadata.create_all(bind = engine) #creates table for categories
 
 @categ_router.post("/",response_model = CategoryResponse,status_code = 201)
 def add_category(categ:CreateCategory,db:Session = Depends(get_db)):
@@ -21,6 +21,8 @@ def get_category(categ_id:int,db:Session = Depends(get_db)):
     if find_category is None:
         raise HTTPException(status_code=404,detail="Category not found!")
     return find_category
+
+
     
 
 
