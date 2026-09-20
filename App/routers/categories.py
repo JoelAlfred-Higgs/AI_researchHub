e2d3703278b7ad3e7083b90ba12d663399e10ca4
JoelAlfred-Higgs <joelalfred.29csa@licet.ac.in>
2026-09-20 11:11:39 +0530

    added two endpoints to deal with updation and deletion

--- App/routers/categories.py
+++ App/routers/categories.py
@@ -1,7 +1,7 @@
-from crud import addcategory,getcategory
+from crud import addcategory,getcategory,updatecategory,deletecategory
 from DataBase import engine,get_db
 from models import Category,Base
-from schemas import CreateCategory, CategoryResponse,UpdateCategory
+from schemas import CreateCategory, CategoryResponse
 from fastapi import APIRouter,HTTPException,Depends
 from sqlalchemy.orm import Session
 
@@ -22,8 +22,19 @@ def get_category(categ_id:int,db:Session = Depends(get_db)):
         raise HTTPException(status_code=404,detail="Category not found!")
     return find_category
 
+@categ_router.put("/{categ_id}",response_model = CategoryResponse,status_code = 200)
+def update_category(categ_id:int,update_category:CategoryResponse,db:Session = Depends(get_db)):
+    updated = updatecategory(categ_id,update_category,db)
+    if updated is None:
+        raise HTTPException(status_code=404,detail="Category not found!")
+    return updated
 
-    
+@categ_router.delete("/{categ_id}",response_model = CategoryResponse,status_code = 200)
+def delete_category(categ_id :int,db:Session = Depends(get_db)):
+    deleted_categ = deletecategory(categ_id,db)
+    if deleted_categ is None:
+        raise HTTPException(status_code=404,detail="Category not found!")
+    return deleted_categ
 
 
 
